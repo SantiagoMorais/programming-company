b9d058b06366f1694611b7b7acb7a59998dddd05
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-25 18:22:32 -0300

    feat(HomePage/RootLayout): configure a temporary logo and change default font to Rubik

--- src/app/components/header/nav-bar/index.tsx
+++ src/app/components/header/nav-bar/index.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import { RoutesList } from "./routes-list";
 import { SheetNavBar } from "./sheet-nav-bar";
 import { ToggleThemeButton } from "./toggle-theme-button";
+import logo from "@/assets/logo.png";
 
 export const NavBar = () => (
   <nav className="flex items-center justify-between">
     <SheetNavBar />
-    <div className="">logo</div>
+    <div className="flex select-none items-center gap-2">
+      <div className="relative size-10">
+        <Image src={logo} fill alt="logo" className="object-contain" />
+      </div>
+      <h1 className="text-3xl font-extrabold">Logo</h1>
+    </div>
     <RoutesList insideASheet={false} className="hidden md:flex" />
     <ToggleThemeButton />
   </nav>
