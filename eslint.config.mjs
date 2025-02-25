fa1254f8209be06c4e76849c5a783f1ee8e1b67a
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-25 13:37:26 -0300

    feat(prettier): configure default formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
+import prettierConfig from "eslint-config-prettier";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +13,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  plugin: {
+    prettierPlugin,
+    typescriptPlugin,
+    "simple-import-sort": simpleImportSort,
+  },
+  rules: {
+    "prettier/prettier": "warn",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  prettierConfig,
+};
 
 export default eslintConfig;
