769bc3d47f3d314be90ef8e34ea8358cdd8ad641
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-26 08:51:16 -0300

    fix(SheetNavBar): ensure sheet will keep closed in small screens

--- src/app/components/header/sheet-nav-bar/index.tsx
+++ src/app/components/header/sheet-nav-bar/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -9,23 +11,39 @@ import {
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import { RoutesList } from "../routes-list";
+import { useEffect, useState } from "react";
 
-export const SheetNavBar = () => (
-  <Sheet>
-    <SheetTrigger asChild>
-      <Button variant="ghost" className="md:hidden md:bg-black">
-        <Menu />
-      </Button>
-    </SheetTrigger>
-    <SheetContent
-      side="left"
-      className="[&>button]:right-8 [&>button]:top-8 [&>button]:scale-150"
-    >
-      <SheetHeader className="relative">
-        <SheetTitle className="text-start text-2xl">Navegação</SheetTitle>
-        <Separator />
-        <RoutesList insideASheet />
-      </SheetHeader>
-    </SheetContent>
-  </Sheet>
-);
+export const SheetNavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768 && open) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [open]);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button variant="ghost" className="md:hidden md:bg-black">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="[&>button]:top-8 [&>button]:right-8 [&>button]:scale-150"
+      >
+        <SheetHeader className="relative">
+          <SheetTitle className="text-start text-2xl">Navegação</SheetTitle>
+          <Separator />
+          <RoutesList insideASheet />
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
