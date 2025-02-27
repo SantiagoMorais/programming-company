c3024f3be5e788c046db715743bd3f105532c3a4
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-26 21:48:06 -0300

    feat(WhyChooseUs): start component configuration

--- src/app/components/content/index.tsx
+++ src/app/components/content/index.tsx
@@ -1,11 +1,13 @@
 import { CoreTechnologies } from "./core-technologies";
 import { FAQs } from "./faqs";
 import { Hero } from "./hero";
+import { WhyChooseUs } from "./why-choose-us";
 
 export const Content = () => (
   <section className="flex h-auto w-full max-w-(--breakpoint-2xl) flex-col items-center gap-20 p-4 py-8 md:gap-10">
     <Hero />
     <CoreTechnologies />
+    <WhyChooseUs />
     <FAQs />
   </section>
 );
---
+++ src/app/components/content/why-choose-us/index.tsx
@@ -0,0 +1,13 @@
+export const WhyChooseUs = () => (
+  <section className="max-w-(--breakpoint-2xl) gap-4">
+    <header className="space-y-4">
+      <h3 className="text-center text-3xl">
+        Por que escolher nossos serviços?
+      </h3>
+      <p className="text-muted-foreground max-w-(--breakpoint-sm) text-center">
+        Desenvolvemos soluções personalizadas para atender às suas necessidades.
+        Descubra como podemos impulsionar o seu negócio!
+      </p>
+    </header>
+  </section>
+);
