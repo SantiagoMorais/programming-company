74d0f8393e91fa983394e647c414e1e5fe181036
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-27 19:41:50 -0300

    feat(env): add environment variable schema validation

---
+++ src/env/index.tsx
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+  LINKEDIN_URL: z.string().url().optional(),
+  INSTAGRAM_URL: z.string().url().optional(),
+  WHATSAPP_NUMBER: z.coerce
+    .string()
+    .min(11)
+    .max(11)
+    .refine((number) => number.length === 11, {
+      message: "WhatsApp number must has 11 characters",
+    }),
+  EMAIL: z.string().email(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) throw new Error("Invalid Environment Variables");
+
+export const env = _env.data;
