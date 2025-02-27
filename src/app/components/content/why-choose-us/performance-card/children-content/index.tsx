e5540c4471eaae0810be7388a42822d1b4dbf573
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-27 18:23:11 -0300

    fix(SkeletonContent): ensure the loaded and loading skeleton content have the same size

--- src/app/components/content/why-choose-us/performance-card/children-content/index.tsx
+++ src/app/components/content/why-choose-us/performance-card/children-content/index.tsx
@@ -9,16 +9,16 @@ import womanProfilePhoto from "@/assets/why-choose-us-section/profile-photo-woma
 import { Skeleton } from "@/components/ui/skeleton";
 
 const LoadingSkeletonContent = () => (
-  <div className="flex w-full flex-col items-center gap-4 md:flex-row">
+  <div className="flex w-full flex-col items-center gap-4 sm:w-fit sm:flex-row">
     <Skeleton className="bg-muted-foreground size-12 min-h-12 min-w-12 rounded-full sm:size-16 sm:min-h-16 sm:min-w-16" />
-    <div className="flex w-full flex-col items-center gap-2 sm:items-start sm:gap-3">
+    <div className="flex w-full flex-col items-center gap-2 sm:w-fit sm:items-start sm:gap-3">
       <Skeleton className="bg-muted-foreground h-6 w-full sm:w-60 md:w-80" />
       <Skeleton className="bg-muted-foreground h-6 w-4/5 sm:w-50 md:w-60" />
     </div>
   </div>
 );
 
-const LoadedSkeletonContent = ({
+const UploadedSkeletonContent = ({
   photo,
   name,
   title,
@@ -27,18 +27,18 @@ const LoadedSkeletonContent = ({
   name: string;
   title: string;
 }) => (
-  <div className="flex flex-col items-center gap-4 space-x-4 sm:flex-row">
+  <div className="flex w-full flex-col items-center gap-4 sm:w-fit sm:flex-row">
     <div className="shadow-secondary border-primary relative size-12 min-h-12 min-w-12 rounded-full border-2 sm:size-16 sm:min-h-16 sm:min-w-16">
       <Image src={photo} alt="foto de perfil" fill className="rounded-full" />
     </div>
-    <div className="flex w-full flex-col items-center gap-2 sm:items-start sm:gap-3">
-      <div className="bg-muted-foreground/30 flex h-6 w-full min-w-0 items-center justify-center rounded-md px-2 sm:w-60 sm:justify-start md:w-80">
-        <p className="truncate text-center text-sm font-semibold text-ellipsis sm:text-start sm:text-base">
+    <div className="flex w-full flex-col items-center gap-2 sm:w-fit sm:items-start sm:gap-3">
+      <div className="bg-muted-foreground/30 flex h-6 w-full items-center justify-center rounded-md sm:w-60 sm:justify-start md:w-80">
+        <p className="truncate px-2 text-center text-sm font-semibold text-ellipsis sm:text-start sm:text-base">
           {name}
         </p>
       </div>
-      <div className="bg-background/50 flex h-6 w-4/5 max-w-4/5 min-w-0 items-center justify-center overflow-hidden rounded-md px-2 sm:w-50 sm:justify-start md:w-60">
-        <p className="text-muted-foreground truncate text-center text-sm text-ellipsis sm:text-start sm:text-base">
+      <div className="bg-background/50 flex h-6 w-4/5 max-w-4/5 items-center justify-center overflow-hidden rounded-md sm:w-50 sm:justify-start md:w-60">
+        <p className="text-muted-foreground truncate px-2 text-center text-sm text-ellipsis sm:text-start sm:text-base">
           {title}
         </p>
       </div>
@@ -77,7 +77,7 @@ export const ChildrenContent = () => {
           animate={{ clipPath: "inset(0 0 0 0)" }}
           exit={{ clipPath: "inset(0 0 0 100%)" }}
           transition={{ duration: 1.2, ease: "easeInOut" }}
-          className="w-full space-y-6"
+          className="flex w-full flex-col items-center gap-6"
         >
           {mousePosition === "out" ? (
             <>
@@ -86,12 +86,12 @@ export const ChildrenContent = () => {
             </>
           ) : (
             <>
-              <LoadedSkeletonContent
+              <UploadedSkeletonContent
                 photo={manProfilePhoto}
                 name="Rafael Henrique Souza"
                 title="Designer UI/UX"
               />
-              <LoadedSkeletonContent
+              <UploadedSkeletonContent
                 photo={womanProfilePhoto}
                 name="Amanda Beatriz Oliveira"
                 title="Desenvolvedora FullStack"
