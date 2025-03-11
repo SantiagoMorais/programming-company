6660fbf187ef5813e8fe8524385f5c4b20eec0b7
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-11 14:00:54 -0300

    feat(portfolio): fetch and display projects by category from hygraph API

--- src/app/components/content/portfolio/index.tsx
+++ src/app/components/content/portfolio/index.tsx
@@ -1,15 +1,31 @@
-"use client";
+import { getProjectsByCategory } from "@/database/hygraph-api";
 
 import { PortfolioList } from "./portfolio-list";
-export const Portfolio = () => (
-  <section className="space-y-4">
-    <h3 className="text-center text-3xl font-semibold md:text-4xl">
-      Veja nossos projetos
-    </h3>
-    <p className="text-muted-foreground text-base">
-      Criamos eccomerces, sites e blogs pessoais até projetos complexos para
-      grandes empresas. Temos todas as soluções que precisar.
-    </p>
-    <PortfolioList />
-  </section>
-);
+export const Portfolio = async () => {
+  const categories = await getProjectsByCategory();
+
+  return (
+    <section className="space-y-4">
+      <h3 className="text-center text-3xl font-semibold md:text-4xl">
+        Veja nossos projetos
+      </h3>
+      <p className="text-muted-foreground text-base">
+        Criamos eccomerces, sites e blogs pessoais até projetos complexos para
+        grandes empresas. Temos todas as soluções que precisar.
+      </p>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <p>{category.categoryName} - lista de projetos: </p>
+            <p>
+              {category.portfolios.map((project) => (
+                <span key={project.id}>{project.description}</span>
+              ))}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <PortfolioList />
+    </section>
+  );
+};
---
+++ src/database/hygraph-api.ts
@@ -0,0 +1,33 @@
+import { IGetProjectsByCategories } from "@/core/interfaces/queries/get-projects-by-portfolio";
+import { env } from "@/env";
+
+export const GET_PROJECTS_BY_CATEGORIES = `
+  query {
+    categories {
+      id
+      categoryName
+      portfolios {
+        id
+        description
+        banner {
+          url
+        }
+      }
+    }
+  }
+`;
+
+export const getProjectsByCategory = async () => {
+  const response = await fetch(env.DATABASE_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${env.DATABASE_TOKEN_ACCESS}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: GET_PROJECTS_BY_CATEGORIES,
+    }),
+  });
+  const json: IGetProjectsByCategories = await response.json();
+  return json.data.categories;
+};
