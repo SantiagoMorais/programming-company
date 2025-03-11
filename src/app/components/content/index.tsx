9c853f7453f739a61d98a3eb827b5ecb977b966e
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-11 18:11:18 -0300

    chore(Content): organizing content order

--- src/app/components/content/index.tsx
+++ src/app/components/content/index.tsx
@@ -10,9 +10,9 @@ export const Content = () => (
   <section className="flex h-auto w-full max-w-(--breakpoint-2xl) flex-col items-center gap-20 p-4 py-8">
     <Hero />
     <AboutUs />
-    <CoreTechnologies />
-    <WhyChooseUs />
     <Portfolio />
+    <WhyChooseUs />
+    <CoreTechnologies />
     <FAQs />
     <AuroraBackground />
   </section>
