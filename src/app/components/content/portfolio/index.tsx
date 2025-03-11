46e280a2a73fef337d6b85b76f5a528c707881b4
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-11 16:13:06 -0300

    chore(Portfolio): adjust component size

--- src/app/components/content/portfolio/index.tsx
+++ src/app/components/content/portfolio/index.tsx
@@ -1,13 +1,14 @@
 import { getProjectsByCategory } from "@/database/hygraph-api";
 
 import { PortfolioList } from "./portfolio-list";
+
 export const Portfolio = async () => {
   const categories = await getProjectsByCategory();
 
   if (!categories.length) return;
 
   return (
-    <section className="space-y-4">
+    <section className="w-full max-w-(--breakpoint-xl) space-y-4">
       <article className="flex flex-col items-center gap-4">
         <h3 className="text-center text-3xl font-semibold md:text-4xl">
           Veja nossos projetos
