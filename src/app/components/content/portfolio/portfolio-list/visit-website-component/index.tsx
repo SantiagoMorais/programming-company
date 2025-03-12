99e8fd2ce67e52c884b00f0691e4f2d0c89a8e37
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-12 14:22:08 -0300

    fix(VisitWebsiteComponent): ensure mobile users can click on project card withou beeing redirect first

--- src/app/components/content/portfolio/portfolio-list/visit-website-component/index.tsx
+++ src/app/components/content/portfolio/portfolio-list/visit-website-component/index.tsx
@@ -1,19 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 
 export const VisitWebsiteComponent = ({
   projectUrl,
 }: {
   projectUrl: string;
-}) => (
-  <div className="group absolute top-0 left-0 z-10 flex h-full w-full cursor-pointer items-center justify-center opacity-0 duration-300 group-hover:opacity-100 group-hover:backdrop-blur-xs">
-    <Button
-      asChild
-      variant="outline"
-      className="hover:border-secondary hover:text-secondary border-2 border-transparent duration-300 hover:opacity-80"
+}) => {
+  const [isMouseIn, setIsMouseIn] = useState<boolean>(false);
+  const handleMousePosition = (mouseIn: boolean) => {
+    setIsMouseIn(mouseIn);
+  };
+
+  return (
+    <div
+      className={`group absolute top-0 left-0 z-10 flex h-full w-full cursor-pointer items-center justify-center opacity-0 duration-300 ${isMouseIn && "opacity-100 backdrop-blur-xs"}`}
+      onMouseEnter={() => handleMousePosition(true)}
+      onMouseLeave={() => handleMousePosition(false)}
     >
-      <a href={projectUrl} target="_blank" className="text-xl font-normal">
-        Visitar Site
-      </a>
-    </Button>
-  </div>
-);
+      <Button
+        asChild
+        variant="outline"
+        className="hover:border-secondary hover:text-secondary border-2 border-transparent duration-300 hover:opacity-80"
+      >
+        <a
+          href={projectUrl}
+          target="_blank"
+          onClick={(e) => {
+            if (!isMouseIn) e.preventDefault();
+          }}
+          className={`text-xl font-normal ${isMouseIn && "hidden"}`}
+        >
+          Visitar Site
+        </a>
+      </Button>
+    </div>
+  );
+};
